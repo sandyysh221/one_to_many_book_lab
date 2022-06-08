0f7ee81e7dd108b93e55660ceac30f38cc630201
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-06-08 17:03:11 +0100

    homepage set up

---
+++ console.py
@@ -0,0 +1,9 @@
+console pdb
+from models.book import Book
+from models.author import Author
+
+import repositories.book_repository as book_repository
+import repositories.author_repository as author_repository
+
+
+pdb.set_trace()
