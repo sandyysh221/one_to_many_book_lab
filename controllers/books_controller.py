ca63dec0f5a4fcfb944dd1812dc0954f69cf34e3
sandyysh221 <102677016+sandyysh221@users.noreply.github.com>
2022-06-08 20:55:19 +0100

    able to create a new book

--- controllers/books_controller.py
+++ controllers/books_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from repositories import author_repository, book_repository
 from models.book import Book
 from flask import Blueprint
@@ -15,4 +15,21 @@ def books():
 @books_blueprint.route("/books/<id>/delete", methods=["POST"])
 def delete_book(id):
     book_repository.delete(id)
+    return redirect("/books")
+
+
+@books_blueprint.route("/books/new", methods=["GET"])
+def book():
+    authors = author_repository.select_all()
+    return render_template("books/new.html", all_authors=authors)
+
+
+@books_blueprint.route("/books", methods=["POST"])
+def create_book():
+    title = request.form["title"]
+    genre = request.form["genre"]
+    author_id = request.form["author_id"]
+    author = author_repository.select(author_id)
+    book = Book(title, genre, author)
+    book_repository.save(book)
     return redirect("/books")
